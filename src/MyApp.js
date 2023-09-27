6de4e93b20247a78ff37c63ee5d741cc273c64fd
Luke Waltz <lukewaltz@outlook.com>
2023-09-27 09:37:40 -0700

    feat: updated myApp and table to step 4 of lab1p2

--- src/MyApp.js
+++ src/MyApp.js
@@ -2,31 +2,39 @@
 import React, {useState} from 'react';
 import Table from "./Table";
 
-const characters = [
-    {
-        name: "Charlie",
-        job: "Janitor",
-    },
-    {
-        name: "Mac",
-        job: "Bouncer",
-    },
-    {
-        name: "Dee",
-        job: "Aspiring Actress",
-    },
-    {
-        name: "Dennis",
-        job: "Bartender",
-    },
-];
-
 function MyApp() {
+    const characters = [characters, setCharacters] = useState([
+        {
+            name: "Charlie",
+            job: "Janitor",
+        },
+        {
+            name: "Mac",
+            job: "Bouncer",
+        },
+        {
+            name: "Dee",
+            job: "Aspiring Actress",
+        },
+        {
+            name: "Dennis",
+            job: "Bartender",
+        },
+    ]);
+
+    function removeOneCharacter (index) {
+	    const updated = characters.filter((character, i) => {
+	        return i !== index
+	    });
+	  setCharacters(updated);
+	}
+
     return (
         <div className="container">
-          <Table characterData={characters} />
-        </div>
-      );
+            <Table characterData={characters} 
+                removeCharacter={removeOneCharacter} />
+        </div>  
+    )
 }
 
 
